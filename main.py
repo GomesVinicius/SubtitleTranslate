d0d23ab1c49043ae207efb1ec9b5873def645d09
Vinicius Gomes Correia <vinicius.correia@zema.com>
2023-09-21 15:53:58 -0300

    Encode UTF-8 add

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ import time
 
 translator = Translator(service_urls=['translate.googleapis.com'])
 
-arch_dest = open('pt-br - example.txt', 'w')
+arch_dest = open('pt-br - example.txt', 'w', encoding='UTF-8')
 
 with open('en - example.txt', 'r') as reader:
     text_array = reader.readlines(-1)
 
 inicio = time.time()
 for x in text_array:
+
     translation = translator.translate(
         text=x,
         dest='pt',
-        # src='unicode'
     )
 
+    # print(translation.text)
     arch_dest.write(translation.text + '\n')
 
 fim = time.time()
